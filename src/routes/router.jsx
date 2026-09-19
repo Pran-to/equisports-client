00bf4f8b198651e270b63b4450fc146798efe7e2
pran-to <prantochakrobortty24@gmail.com>
2026-09-19 23:45:13 +0600

    setup route  and auth context

---
+++ src/routes/router.jsx
@@ -0,0 +1,30 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../layout/MainLayout";
+import Home from "../pages/Home";
+import AllEquipment from "../pages/AllEquipment";
+import AddEquipment from "../pages/AddEquipment";
+import MyEquipment from "../pages/MyEquipment";
+import ViewDetails from "../pages/ViewDetails";
+import UpdateEquipment from "../pages/UpdateEquipment";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import NotFound from "../pages/NotFound";
+import PrivateRoute from "./PrivateRoute";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    errorElement: <NotFound />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/all-equipment", element: <AllEquipment /> },
+      { path: "/equipment/:id", element: <PrivateRoute><ViewDetails /></PrivateRoute> },
+      { path: "/add-equipment", element: <PrivateRoute><AddEquipment /></PrivateRoute> },
+      { path: "/my-equipment", element: <PrivateRoute><MyEquipment /></PrivateRoute> },
+      { path: "/update-equipment/:id", element: <PrivateRoute><UpdateEquipment /></PrivateRoute> },
+      { path: "/login", element: <Login /> },
+      { path: "/register", element: <Register /> },
+    ]
+  }
+]);
